965d780a69febfd0f9bb944300cb18ef52c341ba
devrai21 <63499615+devrai21@users.noreply.github.com>
2023-09-06 12:29:48 -0700

    homework 1

--- SayHello.c
+++ SayHello.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+   int main( int argc, char * argv[] )
+   {
+      printf( "Enter your name: ");
+      char userName[15];
+      gets(userName);
+      printf("Hello, %s", userName);
+   }
